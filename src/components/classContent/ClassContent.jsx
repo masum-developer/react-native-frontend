0d6f1660f20a12c60efda497080b80936b001caf
Masum <fabrika786@gmail.com>
2024-03-06 10:09:37 +0600

    fetch json data

--- src/components/classContent/ClassContent.jsx
+++ src/components/classContent/ClassContent.jsx
@@ -1,13 +1,26 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ClassContent = ({classContent}) => {
-   console.log(classContent)
+const [data,setData] = useState([])
+  useEffect(()=>{
+    fetch('courseContent.json')
+    .then(response => response.json())
+    .then(data => {
+        // Work with the fetched JSON data here
+        console.log(data); // Just an example, you can do whatever you need with the data
+        setData(data)
+  
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+  },[])
 
    const myData = classContent;
   return <Box pb={3}>
+   <Typography variant='h3'>Content will be loaded after some days.</Typography>
 
-{myData}
 
     </Box>;
 
